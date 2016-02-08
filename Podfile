352b6b652d59b22fbcbbbd08dfd090f74975bc2e
Ian Dundas <contact@iandundas.co.uk>
2016-02-08 21:05:43 +0100

    Added RxSwift pods

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Podfile
+use_frameworks!
+
+target 'ReactiveStackView' do
+    pod 'RxSwift',    '~> 2.0'
+    pod 'RxCocoa',    '~> 2.0'
+end
---
+++ Pods/Target Support Files/Pods-ReactiveStackView/Pods-ReactiveStackView-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ReactiveStackViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ReactiveStackViewVersionString[];
+
